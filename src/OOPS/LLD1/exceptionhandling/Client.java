0fe78e07c6b85dc99fc5578aa0cbced414ab897f
Stalin2391 <stalind3@gmail.com>
2026-03-08 19:20:37 +0530

    exception handling initial commit

---
+++ src/OOPS/LLD1/exceptionhandling/Client.java
@@ -0,0 +1,32 @@
+package OOPS.LLD1.exceptionhandling;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Client {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        try {
+            System.out.println("Enter a number:");
+            int a = scanner.nextInt();
+            int b = scanner.nextInt();
+
+            System.out.println("Number: " + a);
+            System.out.println("Number: " + b);
+        }
+        catch (InputMismatchException e) {
+            System.out.println("Invalid input! Please enter a number.");
+        }
+
+        // if user gives 0 then it will throw exception  - ArithmeticException e.g, 10 / 0
+//        System.out.println(a/b);
+
+        // better code have check
+
+//        if(b != 0) {
+//            System.out.println(a/b);
+//        }
+
+        System.out.println("After Exception");
+    }
+}
