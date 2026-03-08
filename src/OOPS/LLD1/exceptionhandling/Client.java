00a45be75ee90b2612b8c888d7dc0b82291cc52a
Stalin2391 <stalind3@gmail.com>
2026-03-08 20:51:14 +0530

    array index bound exception

--- src/OOPS/LLD1/exceptionhandling/Client.java
+++ src/OOPS/LLD1/exceptionhandling/Client.java
@@ -6,27 +6,39 @@ import java.util.Scanner;
 public class Client {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        try {
-            System.out.println("Enter a number:");
-            int a = scanner.nextInt();
-            int b = scanner.nextInt();
-
-            System.out.println("Number: " + a);
-            System.out.println("Number: " + b);
-        }
-        catch (InputMismatchException e) {
-            System.out.println("Invalid input! Please enter a number.");
-        }
+//        try {
+//            System.out.println("Enter a number:");
+//            int a = scanner.nextInt();
+//            int b = scanner.nextInt();
+//
+//            System.out.println("Number: " + a);
+//            System.out.println("Number: " + b);
+//        }
+//        catch (InputMismatchException e) {
+//            System.out.println("Invalid input! Please enter a number.");
+//        }
 
         // if user gives 0 then it will throw exception  - ArithmeticException e.g, 10 / 0
 //        System.out.println(a/b);
 
-        // better code have check
+        // Better code should include proper checks and exception handling.
 
 //        if(b != 0) {
 //            System.out.println(a/b);
 //        }
 
-        System.out.println("After Exception");
+//        System.out.println("After Exception");
+
+        int[] arr = new int[5];
+
+        for(int i = 0; i <= 5; i++){
+            try {
+                System.out.println(arr[i]);
+            }catch (ArrayIndexOutOfBoundsException e){
+                System.out.println("Invalid array index");
+            }
+
+        }
+
     }
 }
